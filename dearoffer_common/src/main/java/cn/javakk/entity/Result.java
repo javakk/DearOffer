4861eb110933aa53cf50e8d13d32cde40cf07cc4
javakk <1037441463@qq.com>
2019-03-23 16:56:34 +0800

    1. 雪花算法id生成器 2. 定义返回结果集、状态码、分页类

---
+++ dearoffer_common/src/main/java/cn/javakk/entity/Result.java
@@ -0,0 +1,64 @@
+package cn.javakk.entity;
+
+/**
+ * @Author: javakk
+ * @Date: 2019/3/23 16:26
+ */
+public class Result {
+    /**
+     * 是否成功
+     */
+    private boolean isSuccess;
+    /**
+     * 返回码
+     */
+    private Integer code;
+    /**
+     * 返回信息
+     */
+    private String message;
+    /**
+     * 数据
+     */
+    private Object data;
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Result(boolean isSuccess, Integer code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result() {};
+}
