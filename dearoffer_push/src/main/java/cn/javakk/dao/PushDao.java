ab15fca91d1907c5dd005c6ed59978ce1a548313
javakk <1037441463@qq.com>
2019-04-07 17:22:21 +0800

    推送微服务基础架构

---
+++ dearoffer_push/src/main/java/cn/javakk/dao/PushDao.java
@@ -0,0 +1,14 @@
+package cn.javakk.dao;
+
+import cn.javakk.entity.Push;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface PushDao extends JpaRepository<Push,String>,JpaSpecificationExecutor<Push>{
+	
+}
---
+++ dearoffer_push/src/main/java/cn/javakk/service/PushService.java
@@ -0,0 +1,135 @@
+package cn.javakk.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Selection;
+
+import cn.javakk.entity.Push;
+import cn.javakk.util.IdWorker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+
+import cn.javakk.dao.PushDao;
+
+/**
+ * 服务层
+ * 
+ * @author Administrator
+ *
+ */
+@Service
+public class PushService {
+
+	private static final String ROLE_ID = "roleId";
+	@Autowired
+	private PushDao pushDao;
+	
+	@Autowired
+	private IdWorker idWorker;
+
+	/**
+	 * 查询全部列表
+	 * @return
+	 */
+	public List<Push> findAll() {
+		return pushDao.findAll();
+	}
+
+	
+	/**
+	 * 条件查询+分页
+	 * @param whereMap
+	 * @param page
+	 * @param size
+	 * @return
+	 */
+	public Page<Push> findSearch(Map whereMap, int page, int size) {
+		Specification<Push> specification = createSpecification(whereMap);
+		PageRequest pageRequest =  PageRequest.of(page-1, size);
+		return pushDao.findAll(specification, pageRequest);
+	}
+
+	
+	/**
+	 * 条件查询
+	 * @param whereMap
+	 * @return
+	 */
+	public List<Push> findSearch(Map whereMap) {
+		Specification<Push> specification = createSpecification(whereMap);
+		return pushDao.findAll(specification);
+	}
+
+	/**
+	 * 根据ID查询实体
+	 * @param id
+	 * @return
+	 */
+	public Push findById(String id) {
+		return pushDao.findById(id).get();
+	}
+
+	/**
+	 * 增加
+	 * @param push
+	 */
+	public void add(Push push) {
+		push.setId( idWorker.nextId()+"" );
+		pushDao.save(push);
+	}
+
+	/**
+	 * 修改
+	 * @param push
+	 */
+	public void update(Push push) {
+		pushDao.save(push);
+	}
+
+	/**
+	 * 删除
+	 * @param id
+	 */
+	public void deleteById(String id) {
+		pushDao.deleteById(id);
+	}
+
+	/**
+	 * 动态条件构建
+	 * @param searchMap
+	 * @return
+	 */
+	private Specification<Push> createSpecification(Map searchMap) {
+
+		return new Specification<Push>() {
+
+			@Override
+			public Predicate toPredicate(Root<Push> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				List<Predicate> predicateList = new ArrayList<Predicate>();
+                
+                // 推送角色
+                if (searchMap.get(ROLE_ID)!=null && !"".equals(searchMap.get(ROLE_ID))) {
+                	predicateList.add(cb.like(root.get(ROLE_ID).as(String.class), "%"+(String)searchMap.get(ROLE_ID)+"%"));
+                }
+				
+				return cb.and( predicateList.toArray(new Predicate[predicateList.size()]));
+
+			}
+		};
+
+	}
+
+}
