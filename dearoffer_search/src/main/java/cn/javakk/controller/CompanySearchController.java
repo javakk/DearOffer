236d744231e13585dd1158af21aed079c4db05d2
javakk <1037441463@qq.com>
2019-05-24 23:01:17 +0800

    CHG '搜索排行记录'

--- dearoffer_search/src/main/java/cn/javakk/controller/CompanySearchController.java
+++ dearoffer_search/src/main/java/cn/javakk/controller/CompanySearchController.java
@@ -6,7 +6,10 @@ import cn.javakk.pojo.Result;
 import cn.javakk.pojo.StatusCode;
 import cn.javakk.service.CompanySearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -21,12 +24,20 @@ public class CompanySearchController {
     @Autowired
     private CompanySearchService companySearchService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Value("${redis.keys.search}")
+    private String searchKey;
+
     @GetMapping(value="/search/{keywords}/{page}/{size}")
     public Result findByTitleLike(@PathVariable String keywords,
                                   @PathVariable int page,
                                   @PathVariable int size){
-        Page<Company> articlePage =
-                companySearchService.search(keywords, page, size);
+        ZSetOperations opsForZSet = redisTemplate.opsForZSet();
+        opsForZSet.incrementScore(searchKey, keywords, 1);
+
+        Page<Company> articlePage = companySearchService.search(keywords, page, size);
         return new Result(true, StatusCode.OK, "查询成功",
                 new PageResult<Company>(articlePage.getTotalElements(), articlePage.getContent()));
     }
--- dearoffer_search/src/main/java/cn/javakk/controller/PositionSearchController.java
+++ dearoffer_search/src/main/java/cn/javakk/controller/PositionSearchController.java
@@ -4,7 +4,10 @@ import cn.javakk.pojo.*;
 import cn.javakk.service.InterviewSearchService;
 import cn.javakk.service.PositionSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -19,10 +22,19 @@ public class PositionSearchController {
     @Autowired
     private PositionSearchService positionSearchService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Value("${redis.keys.search}")
+    private String searchKey;
+
     @GetMapping(value="/search/{keywords}/{page}/{size}")
     public Result findByTitleLike(@PathVariable String keywords,
                                   @PathVariable int page,
                                   @PathVariable int size){
+
+        ZSetOperations opsForZSet = redisTemplate.opsForZSet();
+        opsForZSet.incrementScore(searchKey, keywords, 1);
         Page<Position> articlePage =
                 positionSearchService.search(keywords, page, size);
         return new Result(true, StatusCode.OK, "查询成功",
